35e9de6d4b64ca7364932bf774f6bd78cbd3989c
Scott Stevenson <scotifer92@googlemail.com>
2018-06-17 13:22:29 +0100

    Started index.js from scratch. Adds hard coded test that works for compromised and uncompromised accounts

--- public/index.js
+++ public/index.js
@@ -1,27 +1,26 @@
-const app = function(){
-  query = document.querySelector('.form').value;
-  const url = "https://haveibeenpwned.com/api/v2/breachedaccount/" + query;
+var app = function(){
+  var url = 'https://haveibeenpwned.com/api/v2/breachedaccount/scott_14_@hotmail.co.uk';
   makeRequest(url, requestComplete);
-
 }
 
-const clickButton = document.getElementById("email");
-clickButton.addEventListener('click', app);
-
-const makeRequest = function(url, callback){
-  const request = new XMLHttpRequest();
+var makeRequest = function(url, callback){
+  //create a new XMLHttpRequest object
+  var request = new XMLHttpRequest();
+  //set the type of request we want with the url we want to call
   request.open("GET", url);
-  request.addEventListener("load", callback);
+  //set the callback we want it to use when it has completed the call
+  request.addEventListener('load', callback);
+  //send the request!
   request.send();
 }
 
-const requestComplete = function() {
-  if(this.status === 404) {
-    console.log('No breached accounts');
-  } else if(this.status !== 200){
-    const result = JSON.parse(this.response);
-    console.log(result);
-  }
+var requestComplete = function(){
+  if(this.status === 404) console.log("Nothing to see here");
+  if(this.status !== 200) return;
+  var jsonString = this.responseText;
+  console.log(jsonString);
+  var countries = JSON.parse(jsonString);
+  var country = countries[0];
 }
 
-window.addEventListener('load', app);
+window.addEventListener('load', app)
